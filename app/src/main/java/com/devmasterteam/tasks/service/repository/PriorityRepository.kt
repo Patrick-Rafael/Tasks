b4d02a35c7d4826812e13bc13b051225a6d4d54c
patrick Rafael <patrickrafael05@gmail.com>
2023-01-22 11:53:09 -0300

    Descrição de prioridades

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -13,15 +13,32 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(val context: Context): BaseRepository() {
+class PriorityRepository(val context: Context) : BaseRepository() {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val databasse = TaskDatabase.getDatabase(context).priorityDAO()
 
+    companion object {
+
+        private val cache = mutableMapOf<Int, String>()
+
+        fun getDescription(id: Int): String {
+
+            return cache[id] ?: ""
+        }
+
+        fun setDescription(id: Int, string: String) {
+
+            cache[id] = string
+
+        }
+
+    }
+
     fun list(listener: APIListener<List<PriorityModel>>) {
 
         val call = remote.list()
-        call.enqueue(object : Callback<List<PriorityModel>>{
+        call.enqueue(object : Callback<List<PriorityModel>> {
             override fun onResponse(
                 call: Call<List<PriorityModel>>,
                 response: Response<List<PriorityModel>>
@@ -42,7 +59,25 @@ class PriorityRepository(val context: Context): BaseRepository() {
         return databasse.list()
     }
 
-    fun save (list: List<PriorityModel>){
+    fun getDescription(id: Int): String {
+
+        val cached = PriorityRepository.getDescription(id)
+
+        return if (cached == "") {
+            val description = databasse.getDescription(id)
+            PriorityRepository.setDescription(id, description)
+            description
+        } else {
+
+            cached
+
+        }
+
+
+    }
+
+
+    fun save(list: List<PriorityModel>) {
 
         databasse.clear()
         databasse.save(list)
--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
@@ -8,11 +8,14 @@ import androidx.lifecycle.ViewModel
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.model.ValidationModel
+import com.devmasterteam.tasks.service.repository.PriorityRepository
 import com.devmasterteam.tasks.service.repository.TaskRepository
 
 class TaskListViewModel(application: Application) : AndroidViewModel(application) {
 
     val taskRepository = TaskRepository(application.applicationContext)
+
+    private val priorityRepository = PriorityRepository(application.applicationContext)
 
     private val _tasks = MutableLiveData<List<TaskModel>>()
     val tasks: LiveData<List<TaskModel>> = _tasks
@@ -21,6 +24,11 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
 
         taskRepository.list(object : APIListener<List<TaskModel>> {
             override fun onSuccess(result: List<TaskModel>) {
+
+                result.forEach {
+                    it.priorityDescription = priorityRepository.getDescription(it.priorityId)
+                }
+
                _tasks.value = result
             }
 
